4c0905db49d4ea43bddf6079a2c856d2daa0be73
irigoitiajuanramiro <69129499+irigoitiajuanramiro@users.noreply.github.com>
2020-10-12 15:05:13 -0300

    Add files via upload

---
+++ build/precache-manifest.4d5f4ffb64ca8ea71385d06e1c1fc45a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70488d127917b6ef458a740e28135828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b1b514d1988542001b0",
+    "url": "/static/css/main.69872b03.chunk.css"
+  },
+  {
+    "revision": "dae98f0a3919cfca8afc",
+    "url": "/static/js/2.ca7100ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca7100ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b1b514d1988542001b0",
+    "url": "/static/js/main.fac702d4.chunk.js"
+  },
+  {
+    "revision": "9078284b3c5a7cc4441c",
+    "url": "/static/js/runtime-main.3c7fdb96.js"
+  }
+]);
